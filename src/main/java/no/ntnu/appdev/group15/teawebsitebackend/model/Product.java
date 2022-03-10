95b4cf18e7fd577fe092180bde74285a1c2a9068
Kenneth <Kennetej@stud.ntnu.no>
2022-03-10 12:46:58 +0100

    Created the product classes most of the methods needed for the class are implemented. I have not made the add and remove method yet cuz i have some questions about it.

--- src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/Product.java
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/Product.java
@@ -1,16 +1,94 @@
 package no.ntnu.appdev.group15.teawebsitebackend.model;
 
+import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotAddReviewException;
+
 /**
  * @author Kenneth Misund
  * @version 0.1
  */
 public class Product {
 
+    private long productID;
+    private String productName;
+    private float price;
+    private int amountOfProduct;
+
+    private Details details;
+    private Company company;
+
     /**
      * Makes an instance of the Product class.
      */
-    public Product() {
+    public Product(long id, String productName, float price, int size) {
+        this.productID = id;
 
+        this.productName = productName;
+
+        checkIfProductIsAboveZero(price, "product price");
+        this.price = price;
+
+        if (size < 0) {
+            throw new IllegalArgumentException("amount of products cannot be less than zero");
+        } else {
+            this.amountOfProduct = size;
+        }
+    }
+
+    /**
+     * Gets the unique productID for a product.
+     * @return productID in long.
+     */
+    public long getProductID() {
+        return productID;
+    }
+
+    /**
+     * Gets the product name given for a product.
+     * @return product name.
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * Sets the name for a product.
+     * @param productName to be set.
+     */
+    public void setProductName(String productName) {
+        checkString(productName, "product name");
+        this.productName = productName;
+    }
+
+    /**
+     * Gets  the price given for a product.
+     * @return the price.
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * Gets the size/amount of products in a collection of products.
+     * @return int size/amount.
+     */
+    public int getAmountOfProduct() {
+        return amountOfProduct;
+    }
+
+    /**
+     * Gets the details around a product.
+     * @return the details given.
+     */
+    public Details getDetails() {
+        return details;
+    }
+
+    /**
+     * Gets the Company.
+     * @return the company name.
+     */
+    public Company getCompany() {
+        return company;
     }
 
     /**
@@ -35,6 +113,17 @@ public class Product {
     private void checkIfObjectIsNull(Object object, String error) {
         if (object == null) {
             throw new IllegalArgumentException("The " + error + " cannot be null.");
+        }
+    }
+
+    /**
+     * Checks if an float value is above or zero so that we cannot enter negative values.
+     * @param product the product to be checked.
+     * @param error the message the exception should hold.
+     */
+    private void checkIfProductIsAboveZero(float product, String error) {
+        if (product >= 0) {
+            throw new IllegalArgumentException("The " + error + " Cannot be negative or zero.");
         }
     }
 }
