c0265c66a4bf2a2b2a21ed1b6b1a098601a7d9ef
Kenneth <Kennetej@stud.ntnu.no>
2022-03-11 17:38:34 +0100

    Made the TeaDetails class, with a few methods

--- src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/TeaDetails.java
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/TeaDetails.java
@@ -1,4 +1,51 @@
 package no.ntnu.appdev.group15.teawebsitebackend.model;
 
-public class TeaDetails {
+import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotAddTagException;
+import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotRemoveTagException;
+
+public class TeaDetails implements Details {
+
+
+    public TeaDetails() {
+
+    }
+
+
+    private void checkString(String stringToCheck, String errorPrefix) {
+        checkIfObjectIsNull(stringToCheck, errorPrefix);
+        if (stringToCheck.isEmpty()) {
+            throw new IllegalArgumentException("The " + errorPrefix + " cannot be empty.");
+        }
+    }
+
+    private void checkIfObjectIsNull(Object object, String error) {
+        if (object == null) {
+            throw new IllegalArgumentException("The " + error + " cannot be null.");
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void setDescription(String description) {
+
+    }
+
+    @Override
+    public void addTag(Tag tag) throws CouldNotAddTagException {
+
+    }
+
+    @Override
+    public void removeTag(Tag tag) throws CouldNotRemoveTagException {
+
+    }
+
+    @Override
+    public boolean checkIfTagIsPartOfDetails(Tag tag) {
+        return false;
+    }
 }
