e52aa1839b900cbc7d3fa304ba8b863f32409b39
trine2301 <stavertrine@gmail.com>
2022-04-06 15:45:52 +0200

    created test-class for TeaDetails-class

---
+++ src/test/java/no/ntnu/no/appdev/group15/teawebsitebackend/TeaDetailsTests.java
@@ -0,0 +1,20 @@
+package no.ntnu.no.appdev.group15.teawebsitebackend;
+
+import no.ntnu.appdev.group15.teawebsitebackend.model.Address;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Tests of the teaDetails class.
+ * The following tests are performed:
+ * <ul>
+ *   <li> Successful creation of instance with valid parameters (positive)</li>
+ *   <li> Creation of instance with invalid parameters (negative)</li>
+ * </ul>
+ * @author Trine Merete Staverløkk
+ * @version 0.1
+ */
