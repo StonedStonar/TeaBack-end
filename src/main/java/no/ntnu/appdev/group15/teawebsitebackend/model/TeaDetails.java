3c66c52e5adb05fe90b9ff7d9a3544ea533f48cf
trine2301 <stavertrine@gmail.com>
2022-04-07 11:38:38 +0200

    finished method checkIfTagIsPartOfDetails in Address-class

--- src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/TeaDetails.java
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/TeaDetails.java
@@ -1,5 +1,6 @@
 package no.ntnu.appdev.group15.teawebsitebackend.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotAddTagException;
 import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotRemoveTagException;
@@ -14,8 +15,6 @@ public class TeaDetails implements Details {
 
     private String description;
     private String ingredients;
-    private Tag tag;
-    private Long tagID;
     private List<Tag> tagList;
 
     /**
@@ -27,21 +26,15 @@ public class TeaDetails implements Details {
      * makes an instance of teaDetails.
      * @param description the description of the tea.
      * @param ingredients the ingredients in the tea
-     * @param taglist list of tags
-     *
      */
-    public TeaDetails(String description, String ingredients, List<Tag> taglist){
-        checkIfStringIsEmpty(description, "description");
-        checkIfStringIsNull(description, "description");
+    public TeaDetails(String description, String ingredients){
+        checkString(description, "description");
         this.description = description;
 
-        checkIfStringIsEmpty(ingredients, "ingredients");
-        checkIfStringIsNull(ingredients, "ingredients");
+        checkString(ingredients, "ingredients");
         this.ingredients = ingredients;
 
-        checkIfTagIsPartOfDetails(tag);
-        checkIfTagIsPartOfDetailsWithTagID(tagID);
-        this.tagList = taglist;
+        this.tagList = new ArrayList<>();
     }
 
     /**
@@ -53,12 +46,19 @@ public class TeaDetails implements Details {
     }
 
     /**
+     * Gets the ingredients in the tea
+     * @return the ingredients
+     */
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    /**
      * Sets a description of the tea.
      * @param description a new description.
      */
     public void setDescription(String description) {
-        checkIfStringIsEmpty(description, "ingredients");
-        checkIfStringIsNull(description, "description");
+        checkString(description, "ingredients");
         this.description = description;
     }
 
@@ -69,7 +69,7 @@ public class TeaDetails implements Details {
      */
     @Override
     public void addTag(Tag tagToAdd) throws CouldNotAddTagException {
-        //TODO Should check if parameter is empty or null
+        checkIfObjectIsNull(tagToAdd, "tag to add");
         if (!checkIfTagIsPartOfDetails(tagToAdd)) {
             tagList.add(tagToAdd);
         } else {
@@ -84,6 +84,7 @@ public class TeaDetails implements Details {
      */
     @Override
     public void removeTag(Tag tagToRemove) throws CouldNotRemoveTagException {
+        checkIfObjectIsNull(tagToRemove, "tag to remove");
         if (!checkIfTagIsPartOfDetails(tagToRemove)) {
             tagList.remove(tagToRemove);
         } else {
@@ -97,7 +98,8 @@ public class TeaDetails implements Details {
      * @param errorPrefix the error if the string is invalid.
      * @throws IllegalArgumentException gets thrown if the string is empty or null.
      */
-    private void checkIfStringIsEmpty(String stringToCheck, String errorPrefix) {
+    private void checkString(String stringToCheck, String errorPrefix) {
+        checkIfObjectIsNull(stringToCheck, errorPrefix);
         if(stringToCheck.isEmpty()) {
             throw new IllegalArgumentException("The " + errorPrefix + " cannot be empty.");
         }
@@ -105,12 +107,12 @@ public class TeaDetails implements Details {
 
     /**
      * Checks if a string is invalid by being empty.
-     * @param stringToCheck the String to check.
+     * @param object the String to check.
      * @param errorPrefix the error if the string is invalid.
      * @throws IllegalArgumentException gets thrown if the string is empty or null.
      */
-    private void checkIfStringIsNull(String stringToCheck, String errorPrefix){
-        if (stringToCheck == null) {
+    private void checkIfObjectIsNull(Object object, String errorPrefix){
+        if (object == null) {
             throw new IllegalArgumentException("The " + errorPrefix + " cannot be null.");
         }
     }
@@ -122,18 +124,8 @@ public class TeaDetails implements Details {
      */
     @Override
     public boolean checkIfTagIsPartOfDetails(Tag tag) {
-        return false;
+        boolean valid = tagList.stream().anyMatch(tag1 -> tag.getTagID() == tag1.getTagID());
+        return valid;
     }
-
-    /**
-     * Checks if the tag is part of details with tag ID.
-     * @param tagID the ID of the tag
-     * @return
-     */
-    public boolean checkIfTagIsPartOfDetailsWithTagID(Long tagID) {
-        return false;
-    }
-
-
 
 }
