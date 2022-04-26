3cc9073837b1e80f95b5829b75ad08bd1e866cac
trine2301 <stavertrine@gmail.com>
2022-04-26 12:30:26 +0200

    Created OrderJPA

---
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/database/OrderJPA.java
@@ -0,0 +1,10 @@
+package no.ntnu.appdev.group15.teawebsitebackend.model.database;
+
+/**
+ * Represents a connection to an Object-Relational to the database.
+ * @author Trine Merete Staverløkk
+ * @version 0.1
+ */
+
+public class OrderJPA {
+}
