c80bbcfc9ad5aeb83f9942c9e101bca1e680bc9f
trine2301 <stavertrine@gmail.com>
2022-06-05 18:49:15 +0200

    finished logic for submitChanges-button

--- src/main/java/no/ntnu/appdev/group15/teawebsitebackend/controllers/web/WebCompanyController.java
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/controllers/web/WebCompanyController.java
@@ -1,8 +1,11 @@
 package no.ntnu.appdev.group15.teawebsitebackend.controllers.web;
 
 import javax.servlet.http.HttpSession;
+import no.ntnu.appdev.group15.teawebsitebackend.model.Address;
 import no.ntnu.appdev.group15.teawebsitebackend.model.Company;
+import no.ntnu.appdev.group15.teawebsitebackend.model.CompanyDetails;
 import no.ntnu.appdev.group15.teawebsitebackend.model.database.CompanyJPA;
+import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotAddCompanyException;
 import no.ntnu.appdev.group15.teawebsitebackend.model.exceptions.CouldNotGetCompanyException;
 import no.ntnu.appdev.group15.teawebsitebackend.model.registers.CompanyRegister;
 import org.springframework.security.core.Authentication;
@@ -52,49 +55,81 @@ public class WebCompanyController extends WebController{
      * Get the editCompany page
      */
     @GetMapping("/editCompany")
-    public String getEditCompany(Model model, Authentication authentication, @RequestParam ("companyID") long companyID) {
+    public String getEditCompany(Model model, Authentication authentication, @RequestParam ("companyID") long companyID, HttpSession httpSession)
+        throws CouldNotGetCompanyException {
         addLoggedInAttributes(authentication, model);
-        try {
-            if (companyID == 0) {
-
-            }else {
-                Company company = companyRegister.getCompanyWithId(companyID);
-                model.addAttribute("companyName", company.getCompanyName());
-                model.addAttribute("companyID", company.getCompanyID());
-                model.addAttribute("postalCode", company.getDetails().getAddress().getPostalCode());
-                model.addAttribute("houseNumber", company.getDetails().getAddress().getHouseNumber());
-                model.addAttribute("companyDescription", company.getDetails().getDescription());
-                model.addAttribute("postalPlace", company.getDetails().getAddress().getPostalPlace());
-                model.addAttribute("streetName", company.getDetails().getAddress().getStreetName());
-                model.addAttribute("country", company.getDetails().getAddress().getCountry());
-            }
-        } catch (CouldNotGetCompanyException exception) {
-            //TODO
+        boolean isPreview = httpSession.getAttribute("isPreview") != null ? (boolean) httpSession.getAttribute("isPreview") : false;
+        if (companyID == 0 && !isPreview) {
+            model.addAttribute("companyID", companyID);
+        }else if (!isPreview){
+            Company company = companyRegister.getCompanyWithId(companyID);
+            model.addAttribute("companyName", company.getCompanyName());
+            model.addAttribute("companyID", company.getCompanyID());
+            model.addAttribute("postalCode", company.getDetails().getAddress().getPostalCode());
+            model.addAttribute("houseNumber", company.getDetails().getAddress().getHouseNumber());
+            model.addAttribute("companyDescription", company.getDetails().getDescription());
+            model.addAttribute("postalPlace", company.getDetails().getAddress().getPostalPlace());
+            model.addAttribute("streetName", company.getDetails().getAddress().getStreetName());
+            model.addAttribute("country", company.getDetails().getAddress().getCountry());
         }
+
+        addAllAttributes(model, httpSession);
         return "editCompany";
     }
 
     @PutMapping("/editCompany")
     public RedirectView editCompany (@RequestParam ("companyID") long companyID, @RequestParam ("companyName") String companyName,
-                                     @RequestParam ("companyDescription") String companyDescription, @RequestParam ("postalCode") int postalCode,
-                                     @RequestParam ("postalPlace") String postalPlace, @RequestParam ("streetName") String streetName, @RequestParam ("houseNumber") int houseNumber,
+                                     @RequestParam ("companyDescription") String companyDescription, @RequestParam ("postalCode") String postalCodeAsString,
+                                     @RequestParam ("postalPlace") String postalPlace, @RequestParam ("streetName") String streetName, @RequestParam ("houseNumber") String houseNumberAsString,
                                      @RequestParam ("country") String country, @RequestParam(value = "preview", required = false) String preview, HttpSession httpSession){
         ParameterBuilder parameterBuilder = new ParameterBuilder("editCompany");
         parameterBuilder.addParameter("companyID", Long.toString(companyID));
+        boolean isPreview = preview != null;
+        httpSession.setAttribute("isPreview", isPreview);
+        boolean invalidInput = false;
+        int postalCode = 0;
+        int houseNumber = 0;
         try {
+            postalCode = Integer.parseInt(postalCodeAsString);
+            houseNumber = Integer.parseInt(houseNumberAsString);
             if (companyID == 0 && preview == null){
-
-            }else if (preview != null){
-
+                Address address = new Address(postalCode, postalPlace, streetName, houseNumber, country);
+                CompanyDetails companyDetails = new CompanyDetails(companyDescription, address);
+                Company company = new Company(companyName, companyDetails);
+                companyRegister.addCompany(company);
+                parameterBuilder.addParameter("addedCompany", "true");
+            }else if (isPreview){
+                parameterBuilder.addParameter("isPreview", "true");
             } else {
                 Company company = companyRegister.getCompanyWithId(companyID);
+                Address address = company.getDetails().getAddress();
                 company.setCompanyName(companyName);
                 company.getDetails().setDescription(companyDescription);
-                company.getDetails().getAddress();
-
+                address.setPostalCode(postalCode);
+                address.setPostalPlace(postalPlace);
+                address.setStreetName(streetName);
+                address.setHouseNumber(houseNumber);
+                address.setCountry(country);
+                companyRegister.updateCompany(company);
+                parameterBuilder.addParameter("updatedCompany", "true");
             }
-        } catch (CouldNotGetCompanyException exception) {
+        } catch (CouldNotGetCompanyException | CouldNotAddCompanyException exception) {
+            parameterBuilder.addParameter("invalidCompanyID", "true");
+            invalidInput = true;
+        } catch (IllegalArgumentException exception) {
+            parameterBuilder.addParameter("invalidFields", "true");
+            invalidInput = true;
+        }
 
+        if (isPreview || invalidInput) {
+            httpSession.setAttribute("companyName", companyName);
+            httpSession.setAttribute("companyID", companyID);
+            httpSession.setAttribute("postalCode", postalCode);
+            httpSession.setAttribute("houseNumber", houseNumber);
+            httpSession.setAttribute("companyDescription", companyDescription);
+            httpSession.setAttribute("postalPlace", postalPlace);
+            httpSession.setAttribute("streetName", streetName);
+            httpSession.setAttribute("country", country);
         }
 
 
