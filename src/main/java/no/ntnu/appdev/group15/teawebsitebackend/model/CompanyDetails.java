e81d676c64a4e27d5fea4072010de9b233ddb962
StonedStonar <steinarhjellem@gmail.com>
2022-06-08 23:25:38 +0200

    Fixed the name of a column in company details

--- src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/CompanyDetails.java
+++ src/main/java/no/ntnu/appdev/group15/teawebsitebackend/model/CompanyDetails.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 public class CompanyDetails extends Details{
 
     @OneToOne(targetEntity = Address.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "addressID")
     private Address address;
 
     /**
